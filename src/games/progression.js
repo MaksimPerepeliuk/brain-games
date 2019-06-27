591c2342a2f52bcf2572e7146987ecabc5d64b7d
Max Perepelyuk <kostin7778@gmail.com>
2019-06-27 15:08:49 +0300

    progression algorithm optimization

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,35 +3,21 @@ import getRandomValue from '../random';
 
 const progressionLength = 10;
 
-const makeGameData = (prog, randomIndex) => {
-  const progression = prog;
-  const correctAnswer = progression[randomIndex];
-  progression[randomIndex] = '..';
-  const gameQuestion = progression.join(' ');
-
-  return { gameQuestion, correctAnswer };
-};
-
-const makeProgression = (startProgression, stepProgression) => {
-  const nextProgValue = startProgression + stepProgression;
-  const iter = (progressionElem, acc) => {
-    if (acc.length === progressionLength) {
-      return acc;
-    }
-
-    return iter(progressionElem + stepProgression, [...acc, progressionElem]);
-  };
-
-  return iter(nextProgValue, [startProgression]);
-};
-
 const getGameData = () => {
   const startProgression = getRandomValue();
   const stepProgression = getRandomValue();
-  const progression = makeProgression(startProgression, stepProgression, progressionLength);
-  const randomIndex = getRandomValue(0, progression.length - 1);
+  const hiddenElem = getRandomValue(0, progressionLength - 1);
+  const progression = [];
 
-  return makeGameData(progression, randomIndex);
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression[i] = startProgression + stepProgression * i;
+  }
+
+  const correctAnswer = progression[hiddenElem];
+  progression[hiddenElem] = '..';
+  const gameQuestion = progression.join(' ');
+
+  return { gameQuestion, correctAnswer };
 };
 
 const gameDescription = 'What number is missing in the progression?';
